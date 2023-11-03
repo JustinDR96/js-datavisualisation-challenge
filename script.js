06ade3cf4cdf40f3b84cb001b3c3d5fc15cd415f
Justin <justinderoover@gmail.com>
2023-11-03 15:20:03 +0100

    add second chart

--- script.js
+++ script.js
@@ -56,6 +56,76 @@ new Chart(ctx, {
       borderColor: "rgba(75, 192, 192, 1)",
       borderWith: 1,
       barThickness: 1.5,
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
     })),
   },
 });
+
+// let canvas2 = document.createElement("canvas");
+
+// // Configurez ses attributs
+// canvas2.id = "myChart2"; // ID du canvas
+// canvas2.width = 400; // Largeur du canvas
+// canvas2.height = 200; // Hauteur du canvas
+
+// let table2 = document.getElementById("table2");
+// table.parentNode.insertBefore(canvas2, table2);
+
+// let ctx2 = canvas2.getContext("2d");
+// Créez un élément <canvas> pour le deuxième tableau
+let canvas2 = document.createElement("canvas");
+
+// Configurez les attributs du canvas (ID, largeur, hauteur)
+canvas2.id = "myChart2"; // ID du canvas
+canvas2.width = 400; // Largeur du canvas
+canvas2.height = 200; // Hauteur du canvas
+
+// Obtenez la référence au deuxième tableau (table2)
+let table2 = document.getElementById("table2");
+
+// Extraction des données du deuxième tableau (table2)
+let years2 = [];
+let countries2 = [];
+let cells2 = [];
+
+const headerRow2 = table2.rows[0];
+for (let j = 2; j < headerRow2.cells.length; j++) {
+  years2.push(headerRow2.cells[j].textContent);
+}
+
+for (let i = 1; i < table2.rows.length; i++) {
+  const row2 = table2.rows[i];
+  countries2.push(row2.cells[1].textContent);
+
+  const rowData2 = [];
+  for (let j = 2; j < row2.cells.length; j++) {
+    rowData2.push(parseFloat(row2.cells[j].textContent));
+  }
+  cells2.push(rowData2);
+}
+
+// Créez un objet tableData2 pour les données du deuxième tableau (table2)
+const tableData2 = { years: years2, countries: countries2, cells: cells2 };
+
+const colors = ["#34495e", "#2ecc71"];
+// Créez un deuxième graphique avec Chart.js de type "ligne"
+let ctx2 = canvas2.getContext("2d");
+
+new Chart(ctx2, {
+  type: "bar", // Choisir le type de graphique (bar)
+  data: {
+    labels: tableData2.countries, // Les étiquettes de l'axe des X
+    datasets: tableData2.years.map((year, index) => ({
+      label: year,
+      data: tableData2.cells.map((row) => row[index]),
+      backgroundColor: colors[index], // Utilisez la couleur correspondante
+    })),
+  },
+});
+
+// Ajoutez le canvas2 au-dessus du tableau2
+table2.parentNode.insertBefore(canvas2, table2);
